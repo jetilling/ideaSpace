23e5a5adb4329c5dc22d2a978d959448fe2fa837
jetilling <jetilling@hotmail.com>
2017-03-13 21:11:00 -0600

    started imports, @Component, and html of landing page component

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';
+import { HttpModule }       from '@angular/http';
+
+//Routing
+
+//Modules
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [],
+  bootstrap: []
+})
+
+export class AppModule {}
---
+++ src/app/landingPage/landing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component }      from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'idea-space',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+
+export class landingPageComponent {}
